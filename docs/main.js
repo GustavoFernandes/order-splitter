cd356a565f4dca1b5f7216d2de13a649cf7ae4c8
GustavoFernandes <GustavoFernandes@users.noreply.github.com>
2016-12-05 20:56:32 -0500

    Refactor main.js and add functionality for URL query string input
    - Change indentation from 1 tab to 2 spaces
    - Add some documentation
    - Validate query string
    - Add clarifications to calculations
    - Add table with individual cost breakdown
    - Add hyperlink to share that points back to this order

--- docs/main.js
+++ docs/main.js
@@ -1,42 +1,68 @@
+var OVERHEADS = ['tip', 'tax', 'fee'];
+
 window.onload = init;
 
 function init() {
   // check for URL query parameters
   if (window.location.search) {
-    this.parseQueryStringInput(window.location.search);
+    try {
+      var input = this.parseQueryStringInput(window.location.search);
+      this.split(input);
+    } catch (err) {
+      console.error(err);
+    }
   }
 }
 
-function parseQueryStringInput(queryString) {
-  var queryParams = {};
+/**
+ * Parses a URL query string.
+ *
+ * Below is an example of input:
+ * ?tax=0.30&fee=1.50&tip=15&Gus=5.00
+ */
+function parseQueryStringInput(input) {
+  var map = {
+    persons: {}
+  };
 
-  queryString = queryString.substring(1); // remove prefixing '?'
-  var searchArray = queryString.split('&');
+  input = input.substring(1); // remove prefixing '?'
+  var searchArray = input.split('&');
   for (var i = 0; i < searchArray.length; i++) {
     var pair = searchArray[i].split('=');
-    queryParams[pair[0]] = pair[1];
+    if (OVERHEADS.indexOf(pair[0]) > -1) {
+      map[pair[0]] = Number(pair[1]);
+    } else {
+      map.persons[pair[0]] = Number(pair[1]);
+    }
   }
   
-  if (!queryParams.hasOwnProperty('tax') ||
-      !queryParams.hasOwnProperty('fee') ||
-      !queryParams.hasOwnProperty('tip')) {
-    console.error('Found URL query string but not all required fields are present (tax, tip, and fee)');
-    // example index.html?tax=0.30&fee=1.50&tip=15&gus=5.00
-    return;
+  if (!map.hasOwnProperty('tax') ||
+      !map.hasOwnProperty('fee') ||
+      !map.hasOwnProperty('tip')) {
+    throw 'Found URL query string but not all required fields are present (tax, tip, and fee)';
   }
-  
-  // TODO
+
+  map.tipPercent = map.tip / 100;
+  delete map.tip;
+
+  return map;
 }
 
-// TODO: check if the number at the beginning of the line affects the item cost
-// example: 2 Chicken $4.00
-//   should the cost for the person be $4 or $8?
+/**
+ * Parses the confirmation summary from an OrderUp.com order
+ */
 function parseOrderUpInput(text) {
+  // TODO: check if the number at the beginning of the line affects the item cost
+  // example: 2 Chicken $4.00
+  //   should the cost for the person be $4 or $8?
+
   var LABEL = 'Label for:';
 
-  var map = {};
+  var map = {
+    persons: {}
+  };
   var itemCost = null;
-  var array = input.split('\n');
+  var array = text.split('\n');
   
   for (var i = 0; i < array.length; i++) {
     var line = array[i].trim();
@@ -54,11 +80,11 @@ function parseOrderUpInput(text) {
     if (labelIndex > -1) {
       var person = line.substring(labelIndex + LABEL.length, line.length);
       
-      if (map[person] == null) {
-        map[person] = 0;
+      if (map.persons[person] == null) {
+        map.persons[person] = 0;
       }
       
-      map[person] += itemCost;
+      map.persons[person] += itemCost;
       itemCost = null;
     }
   }
@@ -68,61 +94,143 @@ function parseOrderUpInput(text) {
 
 function onSplitButtonClick() {
   var text = document.getElementById('textarea').value;
-  var taxes = Number(document.getElementById('taxes').value);
-  var fees = Number(document.getElementById('fees').value);
-  var tipPercent = Number(document.getElementById('tip').value);
+
+  var input = this.parseOrderUpInput(text);
+  input.tax = Number(document.getElementById('taxes').value);
+  input.fee = Number(document.getElementById('fees').value);
+  input.tipPercent = Number(document.getElementById('tip').value) / 100;
+
+  this.split(input);
 }
 
-function split() {
+function split(input) {
+  var subtotal = 0;
+  for (var person in input.persons) {
+    subtotal += input.persons[person];
+  }
 
-	// TODO this.parseOrderUpInput(input);
+  var taxPercent = input.tax / subtotal;
+  var feesPerPerson = input.fee / Object.keys(input.persons).length;
+  var tip = input.tipPercent * subtotal;
+  var total = subtotal + input.tax + input.fee + tip;
 
-	var taxPercent = taxes / subtotal;
-	var feesPerPerson = fees / Object.keys(map).length;
-	var tip = tipPercent * subtotal / 100;
-	var total = subtotal + taxes + fees + tip;
+  var costs = {};
+  for (person in input.persons) {
+    costs[person] = input.persons[person] /* item cost */ +
+        input.persons[person] * taxPercent + /* tax on items */ +
+        input.persons[person] * input.tipPercent /* tip on items */ +
+        feesPerPerson;
+  }
 
-	var totalMap = {
-		'Subtotal:': subtotal,
-		'Tax:': taxes,
-		'Fees:': fees,
-		'Tip:': tip,
-		'Total:': total
-	};
+  input.subtotal = subtotal;
+  input.taxPercent = taxPercent;
+  input.feesPerPerson = feesPerPerson;
+  input.tip = tip;
+  input.total = total;
+  input.costs = costs;
 
-	for (var x in map) {
-		map[x] = map[x] + map[x] * taxPercent + feesPerPerson + map[x] * tipPercent / 100;
-	}
-
-	document.getElementById('result').innerHTML = 
-		makePrettyTableFromObject(totalMap) +
-		'<hr>' +
-		'<pre>' + makePrettyTableFromObject(map) + '</pre>';
+  this.display(input);
 }
 
+function display(input) {
+
+  var calculationsTable = '<table>' +
+      '<tr><td>Subtotal:</td><td>$' + this.prettifyNumber(input.subtotal) + '</td><td>(user input; sum of item costs)</td></tr>' +
+      '<tr><td>Tax:</td><td>$' + this.prettifyNumber(input.tax) + '</td><td>(user input)</td></tr>' +
+      '<tr><td>Fees:</td><td>$' + this.prettifyNumber(input.fee) + '</td><td>(user input)</td></tr>' +
+      '<tr><td>Tip:</td><td>$' + this.prettifyNumber(input.tip) + '</td><td>(tip percent * subtotal)</td></tr>' +
+      '<tr><td>Total:</td><td>$' + this.prettifyNumber(input.total) + '</td><td>(subtotal + tax + fees + tip)</td></tr>' +
+      '<tr><td>Fees per Person:</td><td>$' + this.prettifyNumber(input.feesPerPerson) + '</td><td>(fees / number of people)</td></tr>' +
+      '<tr><td>Tax (Percent):</td><td>' + input.taxPercent * 100 + '%</td><td>(tax / subtotal)</td></tr>' +
+      '<tr><td>Tip (Percent):</td><td>' + input.tipPercent * 100 + '%</td><td>(user input)</td></tr>' +
+      '</table>';
+
+  var html =
+      '<hr>' +
+      calculationsTable + '<br>' +
+      this.makeBreakdown(input) + '<br>' +
+      'Publish the following:<br>' +
+      '<pre>' + this.makeTable(input.costs) + '</pre>' +
+      this.makeHyperlink(input.tax, input.fee, input.tipPercent, input.persons);
+
+  document.getElementById('result').innerHTML = html;
+}
+
+/**
+ * Returns a string of a number in the format "#.##"
+ */
 function prettifyNumber(n) {
-	return pad(Math.round(n * 100) / 100);
+  return pad(round(n));
 }
 
+/**
+ * Returns a number rounded to 2 decimals.
+ */
+function round(n) {
+  return Math.round(n * 100) / 100;
+}
+
+/**
+ * Returns a string of a number padded to 2 decimal places
+ */
 function pad(n) {
-	var s = n.toString();
+  var s = n.toString();
 
-	if (s.indexOf('.') == -1) {
-		s += '.';
-	}
+  if (s.indexOf('.') == -1) {
+    s += '.';
+  }
 
-	while (s.length < s.indexOf('.') + 3) {
-		s += '0';
-	}
+  while (s.length < s.indexOf('.') + 3) {
+    s += '0';
+  }
 
-	return s;
+  return s;
 }
 
-function makePrettyTableFromObject(obj) {
-	var table = '<table>';
-	for (var x in obj) {
-		table += '<tr><td>' + x + '</td><td>$' + prettifyNumber(obj[x]) + '</td></tr>';
-	}
-	table += '</table>';
-	return table;
+function makeTable(object) {
+  var table = '<table>';
+  for (var x in object) {
+    table += '<tr><td>' + x + '</td><td>$' + this.prettifyNumber(object[x]) + '</td></tr>';
+  }
+  table += '</table>';
+  return table;
+}
+
+/**
+ * Returns a hyperlink to this split order.
+ * @param {number} tax - amount of taxes
+ * @param {fee} fee - amount of fees
+ * @param {number} tipPercent - tip as a percentage
+ * @param {Object} personItemCosts - map of person name to item costs
+ * @return {string} the hyperlink to this order
+ */
+function makeHyperlink(tax, fee, tipPercent, personItemCosts) {
+  var link = window.location.origin + window.location.pathname;
+  if (link.indexOf('index.html') == -1) {
+    link += 'index.html';
+  }
+
+  link += '?tax=' + tax + '&fee=' + fee + '&tip=' + tipPercent * 100;
+
+  for (var p in personItemCosts) {
+    link += '&' + p + '=' + personItemCosts[p];
+  }
+
+  return '<a href=' + link + '>' + link + '</a>';
+}
+
+function makeBreakdown(input) {
+  var breakdown = '<table id="breakdown">';
+  breakdown += '<tr><th>Person</th><th>Item Costs</th><th>Tax</th><th>Tip</th><th>Fees Per Person</th><th>Person Total</th></tr>';
+  for (var person in input.persons) {
+    breakdown += '<tr><td>' + person + '</td><td>' +
+        input.persons[person] + '</td><td> + ' + // item costs
+        input.persons[person] + ' * ' + input.taxPercent + '</td><td> + ' + // taxes
+        input.persons[person] + ' * ' + input.tipPercent + '</td><td> + ' + // tip
+        input.feesPerPerson + '</td><td> = ' +
+        input.costs[person] + '</td></tr>';
+  }
+
+  breakdown += '</table>';
+  return breakdown;
 }
