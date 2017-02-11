16b56e27565e068cfdb9016621a01a1ec1a7e254
Billy Hardy <hardywb@leidos.com>
2017-02-11 16:57:22 -0500

    add default task

--- gulpfile.js
+++ gulpfile.js
@@ -4,12 +4,14 @@ var deployDir = 'docs';
 var gulp = require('gulp');
 var clean = require('gulp-clean');
 
-gulp.task('clean', function() {
-    return gulp.src(deployDir)
-        .pipe(clean());
-});
+gulp.task('default', ['copy']);
 
 gulp.task('copy', ['clean'], function() {
     return gulp.src(src)
         .pipe(gulp.dest(deployDir));
+});
+
+gulp.task('clean', function() {
+    return gulp.src(deployDir)
+        .pipe(clean());
 });
