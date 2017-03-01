187ed126a9aa6736a9e67173dac1bc2ba02a4b37
GustavoFernandes <GustavoFernandes@users.noreply.github.com>
2017-03-01 12:49:36 -0500

    Clean up gulpfile indentation

--- gulpfile.js
+++ gulpfile.js
@@ -7,36 +7,36 @@ var jshint = require('gulp-jshint');
 
 gulp.task('default', ['copy']);
 
-gulp.task('copy', ['clean', 'lint'], function() {
-    return gulp.src(src)
-        .pipe(gulp.dest(deployDir));
+gulp.task('copy', ['clean', 'lint'], function () {
+  return gulp.src(src)
+      .pipe(gulp.dest(deployDir));
 });
 
-gulp.task('clean', function() {
-    return gulp.src(deployDir)
-        .pipe(clean());
+gulp.task('clean', function () {
+  return gulp.src(deployDir)
+      .pipe(clean());
 });
 
-gulp.task('lint', function() {
-    return gulp.src(src.map(function(path) {
-        return path+'/*.js';
-    }))
-        .pipe(jshint({
-            eqeqeq: true,
-            esversion: 6,
-            eqnull: true
-        }))
-        .pipe(jshint.reporter('jshint-stylish'))
-        .pipe(jshint.reporter('fail'));
+gulp.task('lint', function () {
+  return gulp.src(src.map(function(path) {
+    return path+'/*.js';
+  }))
+      .pipe(jshint({
+        eqeqeq: true,
+        esversion: 6,
+        eqnull: true
+      }))
+      .pipe(jshint.reporter('jshint-stylish'))
+      .pipe(jshint.reporter('fail'));
 });
 
-gulp.task('serve', function() {
-    var browserSync = require('browser-sync');
-    browserSync({
-        server: {
-            baseDir: "./src/"
-        },
-        notify: false
-    });
-    gulp.watch(["./src/*"], browserSync.reload);
+gulp.task('serve', function () {
+  var browserSync = require('browser-sync');
+  browserSync({
+    server: {
+      baseDir: "./src/"
+    },
+    notify: false
+  });
+  gulp.watch(["./src/*"], browserSync.reload);
 });
