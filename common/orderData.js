e40493b41efd43a0e042bc52a361320ec493499b
Billy Hardy <hardywb@leidos.com>
2017-05-23 13:27:55 -0400

    fix fetch path for deploying in subdomain

--- common/orderData.js
+++ common/orderData.js
@@ -1,13 +1,17 @@
 class OrderData {
+    constructor() {
+        this.base = window.location.pathname;
+    }
+
     getData() {
-        return fetch("/data/orderUp.json")
-        .then(stream => stream.body.getReader().read())
-        .then(({value, done}) => this.parse(value));
+        return fetch(this.base+'data/orderUp.json')
+            .then(stream => stream.body.getReader().read())
+            .then(({value, done}) => this.parse(value));
     }
 
     parse(value) {
-        const string = value.reduce((acc, code) => acc += String.fromCharCode(code), "");
-        return JSON.parse(string).map(e => "\n"+e+"\n");
+        const string = value.reduce((acc, code) => acc += String.fromCharCode(code), '');
+        return JSON.parse(string).map(e => '\n'+e+'\n');
     }
 
 }
